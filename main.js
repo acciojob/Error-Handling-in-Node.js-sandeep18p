f11083fd3b701a3ad56d9b71806bea86838f9e3b
sandeep18p <sandeep.pansari98@gmail.com>
2024-04-29 21:44:56 +0530

    oL

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 const fs = require('fs');
 
-const filePath = process.argv[2];
+const file_path = process.argv[2];
 
-fs.readFile(filePath, 'utf8', (err, data) => {
+if (!file_path) {
+  console.error('Usage: node print.js <file_path>');
+  process.exit(1);
+}
+
+fs.readFile(file_path, 'utf8', (err, data) => {
   if (err) {
-    console.error(`Error reading file ${filePath}: ${err}`);
-    return;
-  }
-
-  try {
-    const jsonData = JSON.parse(data);
-    // TODO: Perform error handling for invalid file format and missing data
-  } catch (err) {
-    console.error('Invalid JSON file format. Please provide a valid JSON file.');
+    console.error(`Error: ${err.message}`);
+  } else {
+    console.log(data);
   }
 });

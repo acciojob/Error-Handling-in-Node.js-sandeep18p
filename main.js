a00e8a5668915e5d9f96f524c946a827ee6bb08e
sandeep18p <sandeep.pansari98@gmail.com>
2024-04-29 21:43:43 +0530

    o

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const fs = require('fs');
 
 const filePath = process.argv[2];
-if (!filePath) {
-  console.error('File path don exist');
-}
 
 fs.readFile(filePath, 'utf8', (err, data) => {
-
   if (err) {
     console.error(`Error reading file ${filePath}: ${err}`);
-    return; } else {
-    console.log(data);
+    return;
+  }
+
+  try {
+    const jsonData = JSON.parse(data);
+    // TODO: Perform error handling for invalid file format and missing data
+  } catch (err) {
+    console.error('Invalid JSON file format. Please provide a valid JSON file.');
   }
 });
